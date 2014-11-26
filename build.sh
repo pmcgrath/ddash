51f951426983151fe54fee73b850b76df3160393
pmcgrath <pmcgrath@gmail.com>
2014-11-26 16:02:46 +0000

    Completed build script

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-
 set -e
-set -x
+#set -x
 
 go build
 
-sudo docker build -t pmcgrath/ddash:latest .
+sudo_run_prefix=
+if [ -z "$(groups | grep 'docker1')" ]; then
+	sudo docker build -t ${USER}/ddash:latest .
+	sudo_run_prefix='sudo '
+else
+	docker build -t ${USER}/ddash:latest .
+fi
+
+echo
+echo Use the following to run an instance within a container
+echo -e "\t${sudo_run_prefix}docker run --name=ddash --detach=true --volume=/var/run/docker.sock:/var/run/docker.sock:ro --publish=8090:8090 ${USER}/ddash:latest"
+
